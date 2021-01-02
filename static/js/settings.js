c0d2b251db1cf9e52e3183531f4b457a3d7bdbd9
Julian Weber <github@juwbr.de>
2021-01-02 22:55:09 +0100

    added working force directed graph

---
+++ static/js/settings.js
@@ -0,0 +1,16 @@
+const
+distance_multiplier = 25,
+stroke_width = 1,
+gravity = 0.05,
+
+showPictures = true,
+picture_path = "static/images/",
+picture_file_type = ".png",
+picture_width = 40,
+picture_height = 40,
+
+showText = false;
+
+showCircle = false;
+
+showStrength = false;
